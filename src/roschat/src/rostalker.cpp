53ee869c7c0ccbaaee22cedb46d6b09dda20feae
yang song <ysong.sc@gmail.com>
2015-04-25 20:28:01 -0400

    sample ros talker and listenser program

---
+++ src/roschat/src/rostalker.cpp
@@ -0,0 +1,24 @@
+#include "rostalker.h"
+
+void RosTalker::talk(){
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss<<"hello world";
+    msg.data = ss.str();
+    msg_pub.publish(msg);
+}
+
+int RosTalker::add(int a, int b){
+    return a + b;
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "rosTalker");
+    RosTalker rt;
+    ros::Rate loop_rate(5);
+    while(ros::ok()){
+        rt.talk();
+        loop_rate.sleep();
+    }
+    return 0;
+}
